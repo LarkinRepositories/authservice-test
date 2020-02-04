f55908a7632d25b60c12d8521029bdc4aa1c5524
larkinrepositories <larkingit@yandex.ru>
2020-02-04 17:33:44 +0700

    JwtUserDetailsService

---
+++ src/main/java/com/authservice/authservice/security/JwtUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.authservice.authservice.security;
+
+import com.authservice.authservice.model.User;
+import com.authservice.authservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUserName(username);
+        if (user == null) throw new UsernameNotFoundException("User with username: "+username + "not found");
+        return null;
+    }
+}
